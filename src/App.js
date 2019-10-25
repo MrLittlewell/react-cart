b696621de5dd12a91f460062199dec71a0e04fe6
Ruslan Littlewell <mrlittlewell@gmail.com>
2019-10-25 15:58:58 +0300

    ferst fersion

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Form from './components/Form/Form'
+
+ReactDOM.render(
+  <Form />,
+  document.getElementById('react-cart'),
+)
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import "./style.css"
+import food from "../../images/cart.png"
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: undefined,
+      sale: null
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  checkPromo() {
+
+    let getSale = 20
+
+    this.setState({ sale: getSale });
+  }
+
+  render() {
+
+    let localData = localStorage.getItem('localPrice');
+    let unParse = JSON.parse(localData)
+
+    let price = unParse !== null ? unParse.price : 500
+    let programName = unParse !== null ? unParse.planName : 'Программа не выбрана'
+
+    let haveSale = this.state.sale
+    let getPercent = price - (price * haveSale / 100)
+    console.log(getPercent)
+
+    return (
+      <div className="cart-container">
+        <h2 className="cart-title">{programName}</h2>
+        <a className="cart-to-menu" href="./menu.html">Выбор меню</a>
+        <div className="programm-cart">
+          <div className="form-area"><form>
+            <div className="form-group">
+              <input type="text" name="name" placeholder="Имя" id="cart-name" defaultValue="" required />
+            </div>
+            <div className="form-group double">
+              <input type="text" name="phone" placeholder="Телефон" id="cart-phone" defaultValue="" required />
+              <input type="email" name="email" placeholder="Почта" id="cart-email" defaultValue="" />
+            </div>
+            <div className="form-group">
+              <input type="text" name="address" placeholder="Адрес" id="cart-address" defaultValue="" required />
+            </div>
+            <div className="form-group double">
+              <div className="input-field">
+                <select name="type" className="browser-default" required onChange={this.handleChange}>
+                  <option defaultValue="Наличными курьеру">Наличными курьеру</option>
+                  <option defaultValue="Картой курьеру">Картой курьеру</option>
+                </select>
+              </div>
+              <input type="text" name="promo" placeholder="Промокод" id="cart-promo" defaultValue="" />
+              <a className="check-code" onClick={this.checkPromo}>Check</a>
+            </div>
+            <input type="text" name="price" defaultValue={getPercent} id="cart-price" />
+            <button className="cart__order-button">Заказать</button>
+          </form></div>
+          <div>
+            <h3>Сумма заказа</h3>
+            <div className="info-area">
+              <p>Всего</p>
+              <p><span>{getPercent}</span> BYN</p>
+            </div>
+            <img src={food} alt="cart" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Form;
